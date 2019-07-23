abd0cee504a7809fe38a9f0fed7aa41bc9e4cf79
Maicon José Duarte <duartejosemaicon@gmail.com>
2019-07-23 20:35:52 -0300

    base projeto

---
+++ console/migrations/m190723_233316_create_table_grupo.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190723_233316_create_table_grupo
+ */
+class m190723_233316_create_table_grupo extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m190723_233316_create_table_grupo cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190723_233316_create_table_grupo cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
